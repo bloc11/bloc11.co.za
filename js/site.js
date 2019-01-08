a7ccd8d0e8073159ccc5e65f1e2943f2e6b22d47
Duncan <frsdun@gmail.com>
2019-01-08 10:32:38 +0200

    Move relevant js across to new file
    and place in self-executing-anonymous-functions

---
+++ js/site.js
@@ -0,0 +1,145 @@
+'use strict';
+
+// Nav menu background
+(function () {
+  var didScroll = false;
+
+  window.onscroll = function scroll(e) {
+    didScroll = true;
+  }
+
+  function updateHeaderBG() {
+    if (didScroll) {
+      didScroll = false;
+      if (window.pageYOffset > 50) {
+        document.getElementById('header').classList.add('floating')
+      } else {
+        document.getElementById('header').classList.remove('floating')
+      }
+    }
+  }
+
+  setInterval(updateHeaderBG, 100);
+})();
+
+// Smooth navigation button scrolling
+(function () {
+
+  function scrollToElementSmoothly(element) {
+    var duration = arguments.length <= 1 || arguments[1] === undefined ? 200 : arguments[1];
+    var easing = arguments.length <= 2 || arguments[2] === undefined ? 'linear' : arguments[2];
+    var callback = arguments[3];
+
+    // timing functions
+    var easings = {
+      linear: function linear(t) {
+        return t;
+      },
+      easeInQuad: function easeInQuad(t) {
+        return t * t;
+      },
+      easeOutQuad: function easeOutQuad(t) {
+        return t * (2 - t);
+      },
+      easeInOutQuad: function easeInOutQuad(t) {
+        return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+      },
+      easeInCubic: function easeInCubic(t) {
+        return t * t * t;
+      },
+      easeOutCubic: function easeOutCubic(t) {
+        return --t * t * t + 1;
+      },
+      easeInOutCubic: function easeInOutCubic(t) {
+        return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+      },
+      easeInQuart: function easeInQuart(t) {
+        return t * t * t * t;
+      },
+      easeOutQuart: function easeOutQuart(t) {
+        return 1 - --t * t * t * t;
+      },
+      easeInOutQuart: function easeInOutQuart(t) {
+        return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
+      },
+      easeInQuint: function easeInQuint(t) {
+        return t * t * t * t * t;
+      },
+      easeOutQuint: function easeOutQuint(t) {
+        return 1 + --t * t * t * t * t;
+      },
+      easeInOutQuint: function easeInOutQuint(t) {
+        return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
+      }
+    };
+
+    function checkBody() {
+      document.documentElement.scrollTop += 1;
+      var body = document.documentElement.scrollTop !== 0 ? document.documentElement : document.body;
+      document.documentElement.scrollTop -= 1;
+      return body;
+    }
+
+    var body = checkBody();
+    var start = body.scrollTop;
+    var startTime = Date.now();
+
+    var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+    var destination = documentHeight - element.offsetTop < windowHeight ? documentHeight - windowHeight : element.offsetTop - 50;
+
+    function scroll() {
+      var now = Date.now();
+      var time = Math.min(1, (now - startTime) / duration);
+      var timeFunction = easings[easing](time);
+      body.scrollTop = timeFunction * (destination - start) + start;
+
+      if (body.scrollTop === destination) {
+        if (callback)
+          callback();
+        return;
+      }
+      requestAnimationFrame(scroll);
+    }
+    scroll();
+  }
+
+  // Add onClick events to buttons that scroll smoothly to their href targets
+  var nav_btns = document.querySelectorAll('.js-btn');
+
+  Array.prototype.forEach.call(nav_btns, function (node) {
+    var target_id = node.getAttribute('href').replace('#', '');
+    var target = document.getElementById(target_id);
+    if (target) {
+      node.addEventListener('click', function (e) {
+        e.preventDefault();
+        scrollToElementSmoothly(target, 300, 'easeInQuad');
+      });
+    }
+  });
+
+})();
+
+// Google maps
+(function () {
+  // Must have above : <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAngcWVs020hIadi86aN79U1ibL9qq_c4A"></script>
+  google.maps.event.addDomListener(window, 'load', init);
+  function init() {
+    var mapOptions = {
+      zoom: 15,
+      center: new google.maps.LatLng(-33.911329, 18.471945),
+      styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }]
+    };
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(-33.910382, 18.472563),
+      map: map,
+      title: 'Bloc 11'
+    });
+  }
+
+  document.getElementById('map-overlay').addEventListener('click', function (e) {
+    e.target.classList.add('hide');
+  });
+})();
