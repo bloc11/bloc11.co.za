8f7598f56c5082b4e1b49d220da831766674cd1a
Maciej Fijalkowski <fijall@gmail.com>
2019-05-13 12:01:34 +0200

    work on buttons

--- pay.js
+++ pay.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
 
 });
 
+function recompute() {
+    
+}
+
 function pay() {
     $.get("/pay?reference=" + $("#reference")[0].value.replace(" ", "-"), function (r) {
         eval(JSON.parse(r)['form']);
