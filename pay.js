c01671dcf8490678a773eefe69ca08ad74e2d367
Maciej Fijalkowski <fijall@gmail.com>
2019-05-13 12:13:42 +0200

    write recompute function

--- pay.js
+++ pay.js
@@ -3,8 +3,15 @@ $(document).ready(function () {
 
 });
 
+val = 250;
+
 function recompute() {
-    
+    val = 150;
+    if ($("input[name=tshirt]:checked").val() == "tshirt")
+        val += 100;
+    if ($("input[name=member]:checked").val() != "member")
+        val += 50;
+    $(".paybutton").html("Pay R" + val);
 }
 
 function pay() {
